aee7874e263e5f2f7dab5d700805df988e8b0fc9
thinkpad <atyongsi@163.com>
2020-08-13 10:52:36 +0800

    更改类名

--- src/main/java/org/example/filerepo/ExecWork.java
+++ src/main/java/org/example/filerepo/ExecWork.java
@@ -13,7 +13,7 @@ import org.pentaho.di.trans.TransMeta;
 /**
  * Created by atyongsi@163.com on 2020/8/12
  */
-public class ExecTrans {
+public class ExecWork {
     public static void runTransformation() {
         String fileName = "C:\\workDir\\kettleWork\\File_repository\\清洗一部分数据.ktr";
         try {
